0b18c41f7f89f5e14f79b5b5f74d1eb8373964e0
NicolasBeltranV <Nicoolasb20@gmail.com>
2021-07-19 14:13:25 -0500

    cambios

--- app/src/main/java/com/example/secondaplication/MainActivity2.kt
+++ app/src/main/java/com/example/secondaplication/MainActivity2.kt
@@ -1,12 +1,15 @@
 package com.example.secondaplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.text.InputType
 import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.EditText
 import android.widget.Spinner
-import androidx.core.widget.addTextChangedListener
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -14,37 +17,72 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        var txteditSubTotal = findViewById<EditText>(R.id.inpSubTotal)
+        val txteditSubTotal = findViewById<EditText>(R.id.inpSubTotal)
+        val subtotal = txteditSubTotal.text.toString()
+
+        val feedbacktype = findViewById<Spinner>(R.id.spinnerId)
+        val spinnerOptions = feedbacktype.getSelectedItem().toString()
+
+        val txtiva = findViewById<EditText>(R.id.inpIva2)
+        txtiva.setInputType(InputType.TYPE_NULL);
+
+        val txttotal = findViewById<EditText>(R.id.inpTotal)
+        txttotal.setInputType(InputType.TYPE_NULL);
+
+        val commision = findViewById<Spinner>(R.id.spinCard)
+        val commisionOpt = commision.getSelectedItem().toString()
+
+        val feedbacktypeCont = findViewById<Spinner>(R.id.spinContr)
+        val spinnerOptionsCont = feedbacktypeCont.getSelectedItem().toString()
+
+        val imprent = findViewById<EditText>(R.id.inpImpRent)
+        imprent.setInputType(InputType.TYPE_NULL);
 
 
-        var feedbacktype = findViewById<Spinner>(R.id.spinnerId)
-        var spinnerOptions = feedbacktype.getSelectedItem().toString()
+        val impiva = findViewById<EditText>(R.id.inpIva)
+        impiva.setInputType(InputType.TYPE_NULL);
 
-        var feedbacktypeCont = findViewById<Spinner>(R.id.spinContr)
-        var spinnerOptionsCont = feedbacktypeCont.getSelectedItem().toString()
+        val txttotal2 = findViewById<EditText>(R.id.inpTotalTwo)
+        txttotal2.setInputType(InputType.TYPE_NULL);
 
-        var txttotal = findViewById<EditText>(R.id.inpTotal)
-
-
-        var txtiva = findViewById<EditText>(R.id.inpIva)
+        val subTotal = txteditSubTotal.text.toInt()*0.12
+        //val resultado =
 
 
-        var txttotal2 = findViewById<EditText>(R.id.inpTotalTwo)
 
 
         txteditSubTotal.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 txttotal.setText(s.toString())
+                txttotal2.setText(s.toString())
             }
 
             override fun afterTextChanged(s: Editable?) {
-
             }
 
         })
+
+        feedbacktype.setOnItemSelectedListener(object : OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                when(position){
+                    0 -> {
+                        txtiva.setText("$subTotal")
+                    }
+
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+        })
     }
 }
+
